66be412404e673372b3e00cd10c9c2509edad63d
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2019-12-29 14:33:09 -0500

    adds initializer file to installation

---
+++ lib/generators/templates/universal_track_manager.rb
@@ -0,0 +1,9 @@
+UniversalTrackManager.configure do |config|
+  config.track_ips = true
+  config.track_utms = true
+  config.track_user_agent = true
+
+
+  # config.track_referrer = false
+
+end
--- lib/generators/universal_track_manager/install_generator.rb
+++ lib/generators/universal_track_manager/install_generator.rb
@@ -8,16 +8,20 @@ module UniversalTrackManager
 
     source_root File.expand_path('../templates', __dir__)
 
-
     class_option :orm, type: 'boolean'
+
 
     def self.next_migration_number(path)
       Time.now.utc.strftime("%Y%m%d%H%M%S")
     end
 
     desc "Creates an initializer for Universal Track Manager and copy files to your application."
-    def copy_initializer
+    def create_universal_track_manager_migration
       migration_template "create_universal_track_manager_tables.rb",  "db/migrate/create_universal_track_manager_tables.rb"
+    end
+
+    def create_universal_track_manager_initializer
+      copy_file 'universal_track_manager.rb', 'config/initializers/universal_track_manager.rb'
     end
 
     def migration_version
