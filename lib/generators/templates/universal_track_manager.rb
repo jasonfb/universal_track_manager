a81aa794456b96164c0626c72dde65a4c89cad87
Seth Strumph <seth@seths-mbp.lan>
2021-01-30 14:14:30 +1300

    Rough proposal for making utm params (or more generally, attribution params) configurable at install time.
     * utm_params defult to the standard set: utm_source,utm_campaign,utm_medium,utm_content,utm_term
     * when running generator, defaults can be overridden:
     * eg :  bundle exec rails generate universal_track_manager:install --param_list "utm_source,utm_campaign,utm_medium,utm_content,keyword,match_type"

--- lib/generators/templates/universal_track_manager.rb
+++ lib/generators/templates/universal_track_manager.rb
@@ -2,7 +2,7 @@ UniversalTrackManager.configure do |config|
   config.track_ips = true
   config.track_utms = true
   config.track_user_agent = true
-
+  #GENERATOR INSERTS CAMPAIGN COLUMN CONFIG HERE
 
   # config.track_referrer = false
 
