d0492d086e03a6e80abb71c9318b957fd4296355
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2022-02-13 19:48:22 -0500

    changing name display on campaigns

--- lib/universal_track_manager/models/campaign.rb
+++ lib/universal_track_manager/models/campaign.rb
@@ -2,8 +2,15 @@ class UniversalTrackManager::Campaign < ActiveRecord::Base
   self.table_name = "campaigns"
 
   def name
-    "#{utm_source} #{utm_medium} #{utm_campaign} #{utm_content} #{utm_term}"
+    if try(:gclid_present)
+      "Google Ad"
+    elsif utm_source == "email"
+      "Email: #{utm_medium} #{utm_campaign} #{utm_content}"
+
+    elsif false #facebook / referral  here
+
+    else
+      "Direct: #{utm_source} #{utm_term} #{utm_medium} #{utm_campaign} #{utm_content}"
+    end
   end
-
-
 end
